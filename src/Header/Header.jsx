4f87c916b59e278e804d09f652acffe8c9804d73
kompromiss <artemprodos@gmail.com>
2024-10-21 11:36:56 +0200

    sitecube

---
+++ src/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+export default function Header() {
+    return(
+        <>
+            <header>
+                <nav>
+                    <h1>yes.</h1>
+                    <div>
+                        <p>Sun</p>
+                        <p className='border-mid'>Optical</p>
+                        <p>Fit guide</p>
+                    </div>
+                    <p>Customer care</p>
+                </nav>
+            </header>
+        </>
+    )
+}
